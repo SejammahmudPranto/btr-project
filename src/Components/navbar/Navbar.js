101d5bad7d6db38dd6ce438e5812bcf1fe7e632f
Sejan <prantosm@gmail.com>
2022-07-03 18:01:24 +0600

    navbar finished

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navBar.css";
 import btrLogo from "../../assets/BigetronLogo.svg";
 import shopicon from "../../assets/shopIcon.svg";
+import { FaBars, FaTimes } from "react-icons/fa" ;
 
 const Navbar = ({children}) => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    // eslint-disable-next-line no-unused-vars
+    const handleMenuBar = () => {
+        if (toggleMenu) {
+          setToggleMenu(false);
+        } else {
+          setToggleMenu(true);
+        }
+      }
+
+
   return (
     <>
     <div className='navbar'>
@@ -20,15 +32,35 @@ const Navbar = ({children}) => {
                 <li>Contact</li>
             </nav>
         </div>
-        <div className='shops'>
-            <img src={shopicon} alt='shop icon'/>
-            <li>Shop</li>
+        <div className='nav-right'>
+            
+            <div className='shops'>
+                <img src={shopicon} alt='shop icon'/>
+                <li>Shop</li>
+            </div>
+            <div className='menu-mbl'>
+            {toggleMenu ? <FaTimes onClick={()=> (setToggleMenu(false))}/> : <FaBars onClick={()=> (setToggleMenu(true))}/>}
+            </div>
         </div>
         
+        
     </div>
-    <div className='main'>
-        {children}
-    </div>
+    { toggleMenu ? 
+        <div>
+            <ul className="nav_list-mobile">
+                <li>Teams</li>
+                <li>About</li>
+                <li>Matches</li>
+                <li>News &amp; Events</li>
+                <li>Sponsors</li>
+                <li>Contact</li>
+            </ul>
+        </div> :
+        <div className='main'>
+            {children}
+        </div>
+
+    }
     </>
   )
 }
